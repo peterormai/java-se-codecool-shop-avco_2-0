a5b79d28b8884c3cd38fefc0ac9ccfdb58325b26
Gergely Németh <ngerle@gmail.com>
2017-09-26 12:38:36 +0200

    Add new product category and product to test in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -47,15 +47,26 @@ public class Main {
         Supplier lenovo = new Supplier("Lenovo", "Computers");
         supplierDataStore.add(lenovo);
 
+        Supplier apple = new Supplier( "Apple", "Phones and computers");
+        supplierDataStore.add(apple);
+
         //setting up a new product category
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         productCategoryDataStore.add(tablet);
+
+        ProductCategory mobilePhone = new ProductCategory("Mobile Phone", "Hardware", "A portable telephone that can make and receive calls over a radio frequency link while the user is moving within a telephone service area.");
+        productCategoryDataStore.add(mobilePhone);
 
         //setting up products and printing it
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
 
+        productDataStore.add(new Product("Apple iPhone 8", 799, "USD",
+                "iPhone 8 introduces an all‑new glass design. The world’s most popular camera, now even better. " +
+                        "The smartest, most powerful chip ever in a smartphone. Wireless charging that’s truly effortless. " +
+                        "And augmented reality experiences never before possible. " +
+                        "iPhone 8. A new generation of iPhone.", mobilePhone, apple));
     }
 
 
