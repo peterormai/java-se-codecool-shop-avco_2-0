548fe52780128075bd0ca711a059fbaee8b182a2
Unknown <ormaipeter.fx@gmail.com>
2017-09-27 18:17:38 +0200

    Create new OrderDaoMem class which contains the orders

--- src/main/java/com/codecool/shop/dao/implementation/OrderDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/OrderDaoMem.java
@@ -1,98 +1,43 @@
 package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.dao.OrderDao;
-import com.codecool.shop.model.LineItem;
-import com.codecool.shop.model.Product;
+import com.codecool.shop.model.Order;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class OrderDaoMem implements OrderDao {
-    private static OrderDaoMem ourInstance = null;
-    private static List<LineItem> lineItems = new ArrayList<>();
+public class OrderDaoMem implements OrderDao{
+
+    private static OrderDaoMem orderDaoMemInstance = null;
+    private static List<Order> orders = new ArrayList<>();
 
     public static OrderDaoMem getInstance() {
-        if (ourInstance == null) {
-            ourInstance = new OrderDaoMem();
+        if (orderDaoMemInstance == null) {
+            orderDaoMemInstance = new OrderDaoMem();
         }
-        return ourInstance;
+        return orderDaoMemInstance;
     }
 
     private OrderDaoMem() {
     }
 
     @Override
-    public void add(Product product) {
-        String searchingName = product.getName();
-        boolean isNotIn = true;
-
-        for (LineItem lineItem : lineItems) {
-            if (lineItem.getProductName().equals(searchingName)) {
-                int newQuantity = lineItem.getQuantity() + 1;
-                lineItem.setQuantity(newQuantity);
-                isNotIn = false;
-            }
-        }
-        if (isNotIn) {
-            LineItem newLineItem = new LineItem(product.getId(), product.getName(), product.getDefaultPrice());
-            lineItems.add(newLineItem);
-        }
-        lineItemTest();
-
-    }
-
-    public int numberOfLineItems() {
-        int numberOfItems = 0;
-        for (LineItem lineItem : lineItems) {
-            numberOfItems += lineItem.getQuantity();
-        }
-        return numberOfItems;
-    }
-
-    public static void lineItemTest() {
-        for (LineItem lineItem : lineItems) {
-            System.out.print("id: " + lineItem.getId());
-            System.out.print(" name: " + lineItem.getProductName());
-            System.out.println(" quantity: " + lineItem.getQuantity());
-        }
+    public void add(Order order) {
+        orders.add(order);
     }
 
     @Override
     public void remove(int id) {
-        for (LineItem item: lineItems) {
-            if (item.getId() == id) {
-                lineItems.remove(item);
-                break;
-            }
-        }
+
     }
 
     @Override
-    public List<LineItem> getAll() {
-        return lineItems;
+    public Order get(int id) {
+        return null;
     }
 
-    public float getTotalPrice() {
-        float totalPrice = 0;
-        for (LineItem lineItem : lineItems) {
-            totalPrice += lineItem.getPrice() * lineItem.getQuantity();
-        }
-        return totalPrice;
-    }
-
-    public boolean changeItemValue(String id, String quantity) {
-        int num = Integer.parseInt(quantity);
-        int validId = Character.getNumericValue(id.charAt(1));
-        if (num == 0) {
-            remove(validId);
-            return true;
-        } else {
-            for (LineItem item: lineItems) {
-                if (item.getId() == validId) {
-                    item.setQuantity(num);
-                }
-            }
-            return false;
-        }
+    @Override
+    public List<Order> getAll() {
+        return null;
     }
 }
