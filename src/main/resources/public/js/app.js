4e105d512ab26d3b26e902dfedab702e231b253d
Gergely Németh <ngerle@gmail.com>
2017-10-09 13:25:29 +0200

    Refactor cart review button for direct routing

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     function filter() {
         var url = '/filter?name=' + $(this).data('name') + '&id=' + $(this).data('id') + '&type=' + $(this).data('type');
-        $.get('/filter?name=' + $(this).data('name') + '&id=' + $(this).data('id') + '&type=' + $(this).data('type'),
+        $.get(url,
             function (response) {
                 window.history.pushState('', '', url);
                 var tempDiv = $('<div>').append($.parseHTML(response));
@@ -15,20 +15,12 @@ $(document).ready(function () {
     function addToCart() {
         $.get('/add-to-cart/' + $(this).data('id'), function (response) {
             $('#cartButton').parent().empty().append($.parseHTML(response));
-            $('#cartButton').on("click", reviewCart);
-        })
-    }
-
-    function reviewCart() {
-        $.get('/review-cart', function () {
-            window.location.href = '/review-cart';
         })
     }
 
     function addListeners() {
         $('.filter').on('click', filter);
         $('.addToCart-button').on('click', addToCart);
-        $('#cartButton').on("click", reviewCart)
     }
 
     addListeners()
