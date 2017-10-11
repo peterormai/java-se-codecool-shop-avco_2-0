b94a1a77eda7cb9d4599ab367ddc8db01aebb2b1
hankoattila <hattilla4@gmail.com>
2017-10-11 10:22:28 +0200

    Config file implemented as singleton, it's read a data from text file and create a connection

--- src/main/java/com/codecool/shop/model/Config.java
+++ src/main/java/com/codecool/shop/model/Config.java
@@ -1,36 +1,65 @@
 package com.codecool.shop.model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Config {
+class Config {
+    private String filePath;
     private static Config ourInstance;
+
     private static Connection connection;
 
-    public static Config getInstance() {
+    public static Config getInstance(String filePath) {
         if (ourInstance == null) {
-            ourInstance = new Config();
+            ourInstance = new Config(filePath);
         }
         return ourInstance;
     }
 
-    private Config() {
-        try{
-            connection = getConnection();
+    private Config(String filePath) {
+        this.filePath = filePath;
+        try {
+            connection = createConnectionConfig();
 
-        } catch (Exception e){
-
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
         }
-
     }
 
-    private Connection getConnection() throws SQLException {
+    public static Connection getConnection() {
+        return connection;
+    }
 
+    private Connection createConnectionConfig() throws SQLException {
+        String DATABASE = readIndexOfLines(1);
+        String DB_USER = readIndexOfLines(2);
+        String DB_PASSWORD = readIndexOfLines(3);
+        System.out.println(DATABASE);
         return DriverManager.getConnection(
-                "",
-                "",
-                "");
+                DATABASE,
+                DB_USER,
+                DB_PASSWORD);
+    }
+
+    private String readIndexOfLines(int index) {
+        List<String> outputList = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            while (reader.ready()) {
+                outputList.add(reader.readLine());
+            }
+            reader.close();
+        } catch (IOException io) {
+            throw new IllegalArgumentException(io);
+
+        }
+        return outputList.get(index-1);
     }
 
 
