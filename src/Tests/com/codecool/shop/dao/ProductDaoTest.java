a54465ae385fd48b09fa68d9ec18b3b670473334
Unknown <ormaipeter.fx@gmail.com>
2017-10-11 12:53:19 +0200

    Create ProductDaoTest class

---
+++ src/Tests/com/codecool/shop/dao/ProductDaoTest.java
@@ -0,0 +1,20 @@
+package com.codecool.shop.dao;
+
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ProductDaoTest {
+
+    ProductDao productDao;
+
+
+
+}
