ea7c661a752fa1d45fd95bce5dacbcc40a099b57
Unknown <ormaipeter.fx@gmail.com>
2017-10-12 20:58:38 +0200

    Refactor ProductCategoryDaoTest

--- src/Tests/com/codecool/shop/dao/ProductCategoryDaoTest.java
+++ src/Tests/com/codecool/shop/dao/ProductCategoryDaoTest.java
@@ -2,6 +2,7 @@ package com.codecool.shop.dao;
 
 import com.codecool.shop.dao.implementation.ProductCategoryDAOJdbc;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -72,17 +73,36 @@ class ProductCategoryDaoTest {
     @Test
     void add_whenAddNewProductCategory_shouldStoreThatProductCategory() {
         productCategoryDao.add(productCategory1);
+        int beforeId = productCategoryDao.getAll().get(0).getId();
+        productCategoryDao.remove(beforeId);
+        productCategory2.setId(beforeId + 1);
+        productCategoryDao.add(productCategory2);
         ProductCategory productCategory = productCategoryDao.getAll().get(0);
-        assertEquals(productCategory1, productCategory);
+        assertEquals(convertObjectToList(productCategory2), convertObjectToList(productCategory));
+    }
+
+
+    private List convertObjectToList(ProductCategory productCategory) {
+        List<String> productCategoryList = new ArrayList<>();
+        productCategoryList.add(Integer.toString(productCategory.getId()));
+        productCategoryList.add(productCategory.getName());
+        productCategoryList.add(productCategory.getDepartment());
+        productCategoryList.add(productCategory.getDescription());
+        return productCategoryList;
     }
 
     //
     @Test
     void find_whenSearchForExistingId_shouldFindRelatedProductCategory() {
         productCategoryDao.add(productCategory1);
-        int testId = productCategoryDao.getAll().get(0).getId();
-        ProductCategory productCategory = productCategoryDao.find(testId);
-        assertEquals(productCategory1, productCategory);
+        int beforeId = productCategoryDao.getAll().get(0).getId();
+        productCategoryDao.remove(beforeId);
+
+        productCategoryDao.add(productCategory2);
+        int productCategory2Id = beforeId + 1;
+        productCategory2.setId(productCategory2Id);
+        ProductCategory productCategory = productCategoryDao.find(productCategory2Id);
+        assertEquals(convertObjectToList(productCategory2), convertObjectToList(productCategory));
     }
 
     //
@@ -110,17 +130,20 @@ class ProductCategoryDaoTest {
     @Test
     void remove_whenRemoveProductCategories_shouldRemoveRelatedProductCategory() {
         List<ProductCategory> expectedAllProductCategories = new ArrayList<>();
-        expectedAllProductCategories.add(productCategory2);
 
         productCategoryDao.add(productCategory1);
+        int beforeId = productCategoryDao.getAll().get(0).getId();
         productCategoryDao.add(productCategory2);
 
-        int productCategory1Id = productCategoryDao.getAll().get(0).getId();
-        productCategoryDao.remove(productCategory1Id);
+        productCategoryDao.remove(beforeId);
+
+        int productCategory2Id = beforeId + 1;
+        productCategory2.setId(productCategory2Id);
+        expectedAllProductCategories.add(productCategory2);
 
         List<ProductCategory> allProductCategories = productCategoryDao.getAll();
 
-        assertEquals(expectedAllProductCategories, allProductCategories);
+        assertEquals(convertObjectToList(expectedAllProductCategories.get(0)), convertObjectToList(allProductCategories.get(0)));
     }
 
     //
@@ -147,14 +170,23 @@ class ProductCategoryDaoTest {
     //
     @Test
     void getAll_shouldGiveBackAllProductCategoriesInList() {
-        List<ProductCategory> expectedAllProductCategories = new ArrayList<>();
-        expectedAllProductCategories.add(productCategory1);
-        expectedAllProductCategories.add(productCategory2);
-
+        List<List> expectedAllProductCategories = new ArrayList<>();
         productCategoryDao.add(productCategory1);
         productCategoryDao.add(productCategory2);
 
-        List<ProductCategory> allProductCategories = productCategoryDao.getAll();
+        int productCategory1Id = productCategoryDao.getAll().get(0).getId();
+        int productCategory2Id = productCategoryDao.getAll().get(1).getId();
+
+        productCategory1.setId(productCategory1Id);
+        productCategory2.setId(productCategory2Id);
+
+        expectedAllProductCategories.add(convertObjectToList(productCategory1));
+        expectedAllProductCategories.add(convertObjectToList(productCategory2));
+
+        List<List> allProductCategories = new ArrayList<>();
+        for (ProductCategory prodCat: productCategoryDao.getAll()) {
+            allProductCategories.add(convertObjectToList(prodCat));
+        }
 
         assertEquals(expectedAllProductCategories, allProductCategories);
     }
