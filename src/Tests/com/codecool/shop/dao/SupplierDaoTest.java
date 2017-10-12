423304249bb7d2f5dab670f4ad559570977128aa
Unknown <ormaipeter.fx@gmail.com>
2017-10-12 12:05:02 +0200

    Implement setup in SupplierDaoTest

--- src/Tests/com/codecool/shop/dao/SupplierDaoTest.java
+++ src/Tests/com/codecool/shop/dao/SupplierDaoTest.java
@@ -1,12 +1,17 @@
 package com.codecool.shop.dao;
 
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SupplierDaoTest {
+
+    private SupplierDao supplierDao = SupplierDaoMem.getInstance();
+
     @BeforeEach
     void setUp() {
+        supplierDao.getAll().clear();
     }
 
 }
