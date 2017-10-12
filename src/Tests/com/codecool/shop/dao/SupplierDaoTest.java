890a2bb97320c5825d808f987d00fafcf3480840
Unknown <ormaipeter.fx@gmail.com>
2017-10-12 11:58:30 +0200

    Create SupplierDaoTest

---
+++ src/Tests/com/codecool/shop/dao/SupplierDaoTest.java
@@ -0,0 +1,12 @@
+package com.codecool.shop.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SupplierDaoTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+}
