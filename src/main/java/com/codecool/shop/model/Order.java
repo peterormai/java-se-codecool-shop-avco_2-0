cf93d648d9d04a415d1b10b6cf0e0d2e07b58f54
hankoattila <hattilla4@gmail.com>
2017-10-12 16:19:20 +0200

    LineItemDaoMem implemented, Order merhod changed to getter and setter

--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -1,10 +1,5 @@
 package com.codecool.shop.model;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,128 +7,31 @@ public class Order {
 
     private static Order orderInstance = null;
     private static List<LineItem> lineItems = new ArrayList<>();
-    private OrderStatus status = OrderStatus.NEW;
 
-    public static Order getInstance() {
-        if (orderInstance == null) {
-            orderInstance = new Order();
-        }
+    public static Order getOrderInstance() {
         return orderInstance;
     }
 
-    private Order() {
+    public static void setOrderInstance(Order orderInstance) {
+        Order.orderInstance = orderInstance;
     }
 
-    public void add(Product product) {
-        boolean isNotIn = true;
-
-        for (LineItem lineItem : lineItems) {
-            if (lineItem.getProduct().equals(product)) {
-                int newQuantity = lineItem.getQuantity() + 1;
-                lineItem.setQuantity(newQuantity);
-                isNotIn = false;
-            }
-        }
-        if (isNotIn) {
-            LineItem newLineItem = new LineItem(1,product,1,1);
-            lineItems.add(newLineItem);
-        }
-    }
-
-    public int numberOfItems() {
-        int numberOfItems = 0;
-        for (LineItem lineItem : lineItems) {
-            numberOfItems += lineItem.getQuantity();
-        }
-        return numberOfItems;
-    }
-
-    public void removeLineItem(int id) {
-        for (LineItem item : lineItems) {
-            if (item.getId() == id) {
-                lineItems.remove(item);
-                break;
-            }
-        }
-    }
-
-    public void removeAll() {
-        lineItems = new ArrayList<>();
-    }
-
-
-    public List<LineItem> getAll() {
+    public static List<LineItem> getLineItems() {
         return lineItems;
     }
 
-    public float getTotalPrice() {
-        float totalPrice = 0;
-        for (LineItem lineItem : lineItems) {
-            totalPrice += lineItem.getLineItemsPrice();
-        }
-        return totalPrice;
-    }
-
-    public void changeItemQuantity(String id, String quantity) {
-        int num = Integer.parseInt(quantity);
-        int validId = Integer.parseInt(id);
-        if (num == 0) {
-            removeLineItem(validId);
-        } else {
-            for (LineItem item : lineItems) {
-                if (item.getId() == validId) {
-                    item.setQuantity(num);
-                }
-            }
-        }
-    }
-
-    public void setStatus(OrderStatus status) {
-        System.out.println("Order status set to " + status);
-        this.status = status;
+    public static void setLineItems(List<LineItem> lineItems) {
+        Order.lineItems = lineItems;
     }
 
     public OrderStatus getStatus() {
-        System.out.println(status);
         return status;
     }
 
-    public void JSONFileWrite() throws IOException {
-
-        JSONObject orders = new JSONObject();
-        JSONObject order = new JSONObject();
-        JSONArray lineItemsOfOrder = new JSONArray();
-        for (LineItem lineItem : lineItems) {
-            JSONObject lineItemObj = new JSONObject();
-            JSONObject productObj = new JSONObject();
-            lineItemObj.put("id", lineItem.getId());
-            lineItemObj.put("quantity", lineItem.getQuantity());
-
-            productObj.put("productName", lineItem.getProduct().getName());
-            productObj.put("defaultPrice", lineItem.getProduct().getDefaultPrice());
-            productObj.put("defaultCurrency", lineItem.getProduct().getDefaultCurrency());
-            productObj.put("productCategory", lineItem.getProduct().getProductCategory().getName());
-            productObj.put("supplier", lineItem.getProduct().getSupplier().getName());
-
-            lineItemObj.put("product", productObj);
-            lineItemsOfOrder.add(lineItemObj);
-        }
-
-        orders.put("firstOrder", order);
-        order.put("lineItems", lineItemsOfOrder);
-
-        try (FileWriter file = new FileWriter("src/main/resources/log/orders.txt")) {
-            file.write(orders.toJSONString());
-        }
+    public void setStatus(OrderStatus status) {
+        this.status = status;
     }
 
-    public LineItem getLineItem(int id) {
-        for (LineItem item : lineItems) {
-            if (item.getId() == id) {
-                return item;
-            }
-        }
+    private OrderStatus status = OrderStatus.NEW;
 
-        return null;
-    }
 }
